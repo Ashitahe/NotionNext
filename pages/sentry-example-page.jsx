4f366bbf0afa2ec967d0392811f3772a6afd0507
Asuhe <sphinx@asuhe.org>
2024-08-05 02:33:18 +0800

    feat: support Sentry

--- pages/sentry-example-page.jsx
+++ pages/sentry-example-page.jsx
@@ -1,27 +1,27 @@
-import Head from "next/head";
-import * as Sentry from "@sentry/nextjs";
+import Head from 'next/head';
+import * as Sentry from '@sentry/nextjs';
 
 export default function Page() {
   return (
     <div>
       <Head>
         <title>Sentry Onboarding</title>
-        <meta name="description" content="Test Sentry for your Next.js app!" />
+        <meta name='description" content="Test Sentry for your Next.js app!' />
       </Head>
 
       <main
         style={{
-          minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
-        <h1 style={{ fontSize: "4rem", margin: "14px 0" }}>
+        <h1 style={{ fontSize: '4rem', margin: '14px 0' }}>
           <svg
             style={{
-              height: "1em",
+              height: '1em'
             }}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 200 44"
@@ -35,25 +35,25 @@ export default function Page() {
 
         <p>Get started by sending us a sample error:</p>
         <button
-          type="button"
+          type='button'
           style={{
-            padding: "12px",
-            cursor: "pointer",
-            backgroundColor: "#AD6CAA",
-            borderRadius: "4px",
-            border: "none",
-            color: "white",
-            fontSize: "14px",
-            margin: "18px",
+            padding: '12px',
+            cursor: 'pointer',
+            backgroundColor: '#AD6CAA',
+            borderRadius: '4px',
+            border: 'none',
+            color: 'white',
+            fontSize: '14px',
+            margin: '18px'
           }}
           onClick={async () => {
             await Sentry.startSpan({
               name: 'Example Frontend Span',
               op: 'test'
             }, async () => {
-              const res = await fetch("/api/sentry-example-api");
+              const res = await fetch('/api/sentry-example-api');
               if (!res.ok) {
-                throw new Error("Sentry Example Frontend Error");
+                throw new Error('Sentry Example Frontend Error');
               }
             });
           }}
@@ -62,12 +62,12 @@ export default function Page() {
         </button>
 
         <p>
-          Next, look for the error on the{" "}
-          <a href="https://asuhe.sentry.io/issues/?project=4507720418787328">Issues Page</a>.
+          Next, look for the error on the{' '}
+          <a href='https://asuhe.sentry.io/issues/?project=4507720418787328'>Issues Page</a>.
         </p>
-        <p style={{ marginTop: "24px" }}>
-          For more information, see{" "}
-          <a href="https://docs.sentry.io/platforms/javascript/guides/nextjs/">
+        <p style={{ marginTop: '24px' }}>
+          For more information, see{' '}
+          <a href='https://docs.sentry.io/platforms/javascript/guides/nextjs/'>
             https://docs.sentry.io/platforms/javascript/guides/nextjs/
           </a>
         </p>
